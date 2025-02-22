c5d09683da93caab4e12a0c3fba64b181a648afc
Radhofan <radhofanazizi@gmail.com>
2025-02-22 18:13:45 +0700

    ada

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 # Update package lists and install bc
 sudo apt-get -y update
 sudo apt-get install -y bc
-sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt update
 
 # Install Python dependencies in the Conda environment
