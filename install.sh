d084bb51a14e9e183dd55c0f199b8e9f0ce8640f
Radhofan <radhofanazizi@gmail.com>
2025-02-22 17:51:21 +0700

    ada

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # Update package lists and install bc
 sudo apt-get -y update
 sudo apt-get install -y bc
+sudo apt update
 
 # Install Python dependencies in the Conda environment
 pip --no-cache-dir install antlr4-python2-runtime six astunparse ast pystan edward pyro-ppl==0.2.1 tensorflow==1.5.0 pandas
