5bfd6fad0a5e642636a303f3c68bd7c827e5ab04
Radhofan <radhofanazizi@gmail.com>
2025-02-22 18:18:59 +0700

    ada

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 # Update package lists and install bc
 sudo apt-get -y update
 sudo apt-get install -y bc
+sudo add-apt-repository --remove ppa:webupd8team/java
 sudo apt update
 
 # Install Python dependencies in the Conda environment
 python2 -m pip install --no-cache-dir antlr4-python2-runtime six astunparse ast pystan edward pyro-ppl==0.2.1 tensorflow==1.5.0 pandas
-wget -O corretto.deb https://corretto.aws/downloads/latest/amazon-corretto-8-x64-linux-jdk.deb
-sudo dpkg -i corretto.deb
+sudo apt install -y wget
+wget -O jdk17.deb https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.deb
+sudo dpkg -i jdk17.deb
 python2 -m pip install http://download.pytorch.org/whl/cpu/torch-0.4.0-cp27-cp27mu-linux_x86_64.whl
 
 # Run check.py
