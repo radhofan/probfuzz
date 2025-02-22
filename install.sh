9d57a49192897083563b4535788cd80cbb800886
Radhofan <radhofanazizi@gmail.com>
2025-02-22 18:11:10 +0700

    ada

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@ sudo apt update
 
 # Install Python dependencies in the Conda environment
 python2 -m pip install --no-cache-dir antlr4-python2-runtime six astunparse ast pystan edward pyro-ppl==0.2.1 tensorflow==1.5.0 pandas
-chmod +x ./probfuzz/install_java.sh
-sudo apt install openjdk-8-jdk
+sudo apt install default-jdk -y
 python2 -m pip install http://download.pytorch.org/whl/cpu/torch-0.4.0-cp27-cp27mu-linux_x86_64.whl
 
 # Run check.py
