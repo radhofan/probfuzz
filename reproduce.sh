d2651befd6e95893f592ee13b62e52d98cff387c
Radhofan <radhofanazizi@gmail.com>
2025-02-22 18:31:48 +0700

    ada

--- reproduce.sh
+++ reproduce.sh
@@ -25,8 +25,8 @@ python2 get-pip.py pip==19.3.1
 # Installation
 chmod +x ./probfuzz/install.sh
 bash ./probfuzz/install.sh
-python2 -m pip install install numpy==1.16.6
-python2 -m pip install install antlr4-python2-runtime==4.7.1
+python2 -m pip install numpy==1.16.6
+python2 -m pip install antlr4-python2-runtime==4.7.1
 
 # Run ProbFuzz
 python2 probfuzz/probfuzz.py 5
