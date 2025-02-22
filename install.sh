214c249b9fc1430f0571fa1db47cc0804a64ef1e
Radhofan <radhofanazizi@gmail.com>
2025-02-22 18:34:53 +0700

    ada

--- install.sh
+++ install.sh
@@ -6,9 +6,7 @@ sudo apt update
 
 # Install Python dependencies in the Conda environment
 python2 -m pip install --no-cache-dir antlr4-python2-runtime six astunparse ast pystan edward pyro-ppl==0.2.1 tensorflow==1.5.0 pandas
-sudo apt install -y wget
-wget -O jdk17.deb https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.deb
-sudo dpkg -i jdk17.deb
+sudo apt install -y openjdk-17-jdk
 python2 -m pip install http://download.pytorch.org/whl/cpu/torch-0.4.0-cp27-cp27mu-linux_x86_64.whl
 
 # Run check.py
