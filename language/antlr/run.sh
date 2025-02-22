4a25d34422205576cdef1d92f19b1203ba9b2fa0
Radhofan <radhofanazizi@gmail.com>
2025-02-23 01:44:38 +0700

    ada

--- language/antlr/run.sh
+++ language/antlr/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 antlr4='java -Xmx500M -cp ".:probfuzz/language/antlr/antlr-4.7.1-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
 grun='java org.antlr.v4.gui.TestRig'
-$antlr4 -package "tool.parser"  -Dlanguage=Python2 -visitor Template.g4
+$antlr4 -package "tool.parser"  -Dlanguage=Python2 -visitor probfuzz/language/antlr/Template.g4
 touch __init__.py
