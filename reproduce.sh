6170693e68cbcb73c0fdbfaa1f1ff308803d2c0e
Radhofan <radhofanazizi@gmail.com>
2025-03-07 16:21:45 +0700

    ada

--- reproduce.sh
+++ reproduce.sh
@@ -3,6 +3,7 @@
 sudo apt update
 sudo apt install -y openjdk-17-jdk
 java -version
+sudo apt install -y python3-pip
 python3 -m pip install numpy
 sudo ./probfuzz/install.sh
 ./probfuzz/probfuzz.py 5
